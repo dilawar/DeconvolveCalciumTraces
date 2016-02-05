d96dd089ea614ceb68ac3dd0d694213bd28570d5
seethakris <seethakris@gmail.com>
2016-02-05 17:47:27 +0530

    Add text function

--- getfiringrate.py
+++ getfiringrate.py
@@ -9,20 +9,21 @@ from constrained_foopsi import constrained_foopsi
 
 # Load calcium trace given mat file and convert to CSV
 class LoadCalciumTrace(object):
-    def __init__(self, WorkingDirectory, FileName):
+    def __init__(self, WorkingDirectory, FileName, key):
         self.WorkingDirectory = WorkingDirectory
         self.FileName = FileName
+        self.key = key
 
     def get_matfile_convert_to_csv(self):
         alldata = scipy.io.loadmat(os.path.join(self.WorkingDirectory, self.FileName))  # Load data
-        calcium = alldata['A_On_SubClusters']
+        calcium = alldata[self.key]
 
         compiledata = []
         for ii in xrange(0, np.size(calcium, 1)):  # Compile from all fish
             compiledata.append(calcium[0, ii][11:, :])
 
         compiledata = np.asarray(np.hstack(compiledata))
-        np.savetxt(os.path.join(self.WorkingDirectory, "rawdata.csv"), compiledata, delimiter=",")
+        np.savetxt(os.path.join(self.WorkingDirectory, key + "_rawdata.csv"), compiledata, delimiter=",")
 
         print np.shape(compiledata)
         return compiledata
@@ -47,17 +48,25 @@ class LoadCalciumTrace(object):
         firingrate = np.asarray(np.vstack(firingrate)).T
         denoisedcalicumtrace = np.asarray(np.vstack(denoisedcalicumtrace)).T
 
-        np.savetxt(os.path.join(self.WorkingDirectory, "denoiseddata.csv"), denoisedcalicumtrace, delimiter=",")
-        np.savetxt(os.path.join(self.WorkingDirectory, "firingrate.csv"), firingrate, delimiter=",")
+        np.savetxt(os.path.join(self.WorkingDirectory, key + "_denoiseddata.csv"), denoisedcalicumtrace, delimiter=",")
+        np.savetxt(os.path.join(self.WorkingDirectory, key + "_firingrate.csv"), firingrate, delimiter=",")
 
-        plt.plot(firingrate, '.')
+        plt.plot(np.clip(firingrate, 0, np.max(firingrate)), '.')
         plt.savefig('/Users/seetha/Desktop/test1.png')
+
+    def test(self):
+        firingrate = np.loadtxt(os.path.join(self.WorkingDirectory, "firingrate.csv"), delimiter=",")
+        print 'Max ', np.max(firingrate), 'Min ', np.min(firingrate)
+        plt.plot(np.clip(firingrate, 0, np.max(firingrate)), '.')
+        plt.savefig('/Users/seetha/Desktop/test2.png')
 
 
 if __name__ == '__main__':
     DataPath = '/Users/seetha/Desktop/Modelling/Data/Blue_Con_Fish104-109.mat'
-    WorkingDirectory, FileName = os.path.split(DataPath)
+    key = 'A_Off_SubClusters'
 
-    Habenula = LoadCalciumTrace(WorkingDirectory, FileName)
+    WorkingDirectory, FileName = os.path.split(DataPath)
+    Habenula = LoadCalciumTrace(WorkingDirectory, FileName, key)
     CalciumData = Habenula.get_matfile_convert_to_csv()
     Habenula.convert_to_firing_rate(CalciumData)
+    # Habenula.test()
