65f3c9b6328944a2f563f0bd9a953632e155e0e2
Dilawar Singh <dilawars@ncbs.res.in>
2016-02-14 20:28:42 +0530

    Interneurons frequency increases as soon as one neuron is diabled. which is quite cool.

--- minimal_network.py
+++ minimal_network.py
@@ -17,11 +17,10 @@ __status__           = "Development"
 from brian2 import *
 import pylab 
 import sys
+import networkx as nx
 
 
 """
-
-
 A network with rapheN raphe neurons and interN interneurons. Each rapheN
 neurons recive an external excitatory synapse. If one of those input is
 shutdown, this network should shut down.
@@ -32,11 +31,31 @@ rapheN = 9
 interN = 9
 inputN = rapheN - 1
 
+# Add the nodes.
+graph_ = nx.DiGraph( ranksep = '1.0', nodesep = '1.0' )
+
+rapheNode =  [ ('RA%d' % x) for x in range(rapheN) ] 
+interNode = [ ('IN%d' % x) for x in range(interN) ] 
+inputNode = [ ('i_%d' % x) for x in range(inputN) ] 
+
+for n in rapheNode:
+    graph_.add_node( n, label = n, color = 'blue' )
+for n in interNode:
+    graph_.add_node( n, label = n, color = 'red' )
+for n in inputNode:
+    graph_.add_node( n, label = n,  color = 'yellow' )
+
+for e in zip(inputNode, rapheNode):
+    graph_.add_edge( *e )
+
+for e in zip(rapheNode, interNode):
+    graph_.add_edge( *e , arrowhead = 'diamond' )
+
 monitors = []
 
-footer = ""
+titleText = ""
 
-shutdown = 4.5
+shutdown = 1.5
 stimulus = TimedArray(np.array([45, 58]), dt=shutdown*second)
 
 inputNet = NeuronGroup( inputN 
@@ -62,8 +81,8 @@ inputWithClamp = NeuronGroup( 1
         , threshold='v > -50*mV'
         , reset='v = -70*mV'
         )
-footer += "Among all input neurons, one neuron shuts down at %s sec" % shutdown
-footer += " shown by red X"
+titleText += "Among all input neurons, one neuron shuts down at %s sec" % shutdown
+titleText += " shown by red X"
 
 inputMonitor = SpikeMonitor( inputNet )
 inputClampedMonitor = SpikeMonitor( inputWithClamp )
@@ -74,12 +93,12 @@ inputClampedMonitor = SpikeMonitor( inputWithClamp )
 # fast as 35Hz.
 interneuronNet = NeuronGroup( interN
         , '''
-            dv/dt = (ge+gi-(v+49*mV))/(6*ms) : volt
-            dge/dt = -ge/(2*ms) : volt
-            dgi/dt = -gi/(10*ms) : volt
+            dv/dt = (ge+gi-(v+45*mV))/(20*ms) : volt
+            dge/dt = -ge/(7*ms) : volt
+            dgi/dt = -gi/(5*ms) : volt
         '''
-        , threshold='v >-50*mV'
-        , reset = 'v = -70*mV'
+        , threshold='v > -46*mV'
+        , reset = 'v = -60*mV'
         )
 interMonitor = SpikeMonitor( interneuronNet )
 
@@ -109,28 +128,60 @@ inputSyn1.connect( 'j==0' )
 raphe2Interneurons = Synapses( rapheNet, interneuronNet, pre='gi-=10*mV')
 raphe2Interneurons.connect( 'i==j' )
 
-# Interneurons makes weak inhibtory synapses onto raphe, These connections are
-# dense.
-inter2Raphe = Synapses( interneuronNet, rapheNet, pre='gi-=2.62*mV' )
-inter2Raphe.connect( True, p= 1.0)
+# Interneurons makes weak inhibtory synapses onto raphe. In this version, these
+# are sparse. To make sure that effect is similar to previous version, one
+# hypothise that interneurons are exciting their neighbours.
+inter2Raphe = Synapses( interneuronNet, rapheNet, pre='gi-=2*mV' )
+probOfConnection = 0.9
+inter2Raphe.connect( 'abs(i-j) <=4 and i!=j', p=probOfConnection)
+
+# excite your neighbour
+inter2interExc = Synapses( interneuronNet, interneuronNet, pre='ge+=4*mV')
+inter2interExc.connect( 'abs(i-j)<=2 and i!=j' )
 
 
-run( 10*second )
-marker = '.'
-pylab.subplot(4, 1, 2)
-pylab.plot( inputMonitor.t, inputMonitor.i, marker)
-plot( inputClampedMonitor.t, inputClampedMonitor.i, 'x' )
-pylab.title( 'Input spikes')
 
-pylab.subplot(4, 1, 3)
-plot( interMonitor.t, interMonitor.i, marker ) 
 
-pylab.subplot(4, 1, 4)
-plot( rapheMonitor.t, rapheMonitor.i, marker ) 
-pylab.title( 'Raphe neurons' )
-pylab.suptitle( footer, fontsize=8, verticalalignment = 'bottom' )
-pylab.tight_layout()
-outfile = '%s.png' % sys.argv[0]
-print('[INFO] Writing output to %s' % outfile)
-pylab.savefig( "%s.png" % sys.argv[0] )
+titleText += '\nProbabilty I --| R = %s, neighbourhood=%s' %(probOfConnection,4)
+stamp = datetime.datetime.now().isoformat()
+titleText += '\nSimulated on: \n%s' % stamp
 
+# Connect in graph as well.
+for n in interNode:
+    for nn in rapheNode:
+        graph_.add_edge( n, nn, arrowshape='tee' )
+
+def simulate( runtime ):
+    run( runtime*second )
+    marker = '.'
+    pylab.subplot(4, 1, 2)
+    pylab.plot( inputMonitor.t, inputMonitor.i, marker)
+    plot( inputClampedMonitor.t, inputClampedMonitor.i, 'x' )
+    pylab.title( 'Input spikes')
+
+    pylab.subplot(4, 1, 3)
+    plot( interMonitor.t, interMonitor.i, marker ) 
+
+    pylab.subplot(4, 1, 4)
+    plot( rapheMonitor.t, rapheMonitor.i, marker ) 
+    pylab.title( 'Raphe neurons' )
+    pylab.suptitle( titleText, fontsize=8, verticalalignment = 'top' )
+    pylab.tight_layout()
+    outfile = '%s.png' % sys.argv[0]
+    print('[INFO] Writing output to %s' % outfile)
+    pylab.savefig( "%s.png" % sys.argv[0] )
+
+def write_graphviz( ):
+    # Generate a networkx graph which represent what we are simulating.
+    global graph_ 
+    # Add connections here and we are done.
+    nx.write_dot( graph_, 'network.dot' )
+    #nx.draw( graph_ )
+    #pylab.show()
+
+def main( ):
+    simulate( runtime = 4 )
+    #write_graphviz( )
+
+if __name__ == '__main__':
+    main()
